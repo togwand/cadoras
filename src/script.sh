aef4a0a9baaac92342a51f2f45f7b1b8e3c08dba
togwand <togwand@gmail.com>
2024-12-09 18:11:31 -0300

    Removed mdsh from the formatters as with goris, and also swapped bat for less on the git diff option, because reasons...

--- src/script.sh
+++ src/script.sh
@@ -150,7 +150,7 @@ EOF
   o2() {
     full-diff() {
       read-args "git add" "--all" confirm
-      read-args "git diff HEAD" "| bat" confirm
+      read-args "git diff HEAD" "| less" confirm
     }
     if is-user
     then
